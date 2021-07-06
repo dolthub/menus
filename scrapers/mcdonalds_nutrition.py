f7246e2af3032591f0c08cca6f1d8636f6399024
Katie McCulloch <katie@dolthub.com>
2021-07-06 10:15:07 -0700

    Add remaining columm processing for mcdonalds menu items nutritional info

--- scrapers/mcdonalds_nutrition.py
+++ scrapers/mcdonalds_nutrition.py
@@ -8,7 +8,7 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 db = doltcli.Dolt("../../../")
 
-menuCategories = ["burgers", "sandwhiches", "wraps","chicken","salads","breakfast","sides","drinks"]
+menuCategories = ["burgers & sandwhiches", "wraps","chicken","salads","breakfast","sides","drinks"]
 
 items = soup.find_all("td", class_="column-1")
 calories = soup.find_all("td", class_="column-2")
@@ -22,7 +22,9 @@ filteredItems = []
 
 def itemIsCategory(item):
     for category in menuCategories:
-        if category.lower() in item.lower():
+        item = item.lower().strip()
+        if category.lower() == item:
+            print(item.lower())
             return True
         else:
             continue
@@ -59,6 +61,56 @@ def getFatG(item):
         fat = str(item.nextSibling.nextSibling.next_element["value"])
     return fat.replace(" ", "")
 
+def getCholesterol(item):
+    cholTd = item.nextSibling.nextSibling.nextSibling.next_element.string
+    cholTdString = str(cholTd)
+    cholesterol = ""
+    if cholTdString != "None":
+        cholesterol = cholTdString.replace("mg", "")
+    if str(cholTdString) == "None":
+        cholesterol = str(item.nextSibling.nextSibling.nextSibling.next_element["value"])
+    return cholesterol.replace(" ", "")
+
+def getCarbs(item):
+    carbTd = item.nextSibling.nextSibling.nextSibling.nextSibling.next_element.string
+    carbTdString = str(carbTd)
+    carb = ""
+    if carbTdString != "None":
+        carb = carbTdString.replace("g", "")
+    if str(carbTdString) == "None":
+        carb = str(item.nextSibling.nextSibling.nextSibling.nextSibling.next_element["value"])
+    return carb.replace(" ", "")
+
+def getFiber(item):
+    carbTd = item.nextSibling.nextSibling.nextSibling.nextSibling.next_sibling.next_element.string
+    carbTdString = str(carbTd)
+    carb = ""
+    if carbTdString != "None":
+        carb = carbTdString.replace("g", "")
+    if str(carbTdString) == "None":
+        carb = str(item.nextSibling.nextSibling.nextSibling.nextSibling.next_sibling.next_element["value"])
+    return carb.replace(" ", "")
+
+def getSugar(item):
+    carbTd = item.nextSibling.nextSibling.nextSibling.nextSibling.next_sibling.next_sibling.next_element.string
+    carbTdString = str(carbTd)
+    carb = ""
+    if carbTdString != "None":
+        carb = carbTdString.replace("g", "")
+    if str(carbTdString) == "None":
+        carb = str(item.nextSibling.nextSibling.nextSibling.nextSibling.next_sibling.next_sibling.next_element["value"])
+    return carb.replace(" ", "")
+
+def getProtein(item):
+    carbTd = item.nextSibling.nextSibling.nextSibling.nextSibling.next_sibling.next_sibling.next_sibling.next_element.string
+    carbTdString = str(carbTd)
+    carb = ""
+    if carbTdString != "None":
+        carb = carbTdString.replace("g", "")
+    if str(carbTdString) == "None":
+        carb = str(item.nextSibling.nextSibling.nextSibling.nextSibling.next_sibling.next_sibling.next_sibling.next_element["value"])
+    return carb.replace(" ", "")
+
 
 def buildRows():
     rows = []
@@ -71,6 +123,11 @@ def buildRows():
         else:
             calories = getCalories(item)
             fat = getFatG(item)
+            cholesterol = getCholesterol(item)
+            carbs = getCarbs(item)
+            fiber = getFiber(item)
+            sugar = getSugar(item)
+            protein = getProtein(item)
 
             # skip category/header rows
             itemDict = {
@@ -78,14 +135,19 @@ def buildRows():
                 "restaurant_name": "McDonald's",
                 "identifier": "National",
                 "calories": calories,
-                "fat(g)": fat
+                "fat(g)": fat,
+                "cholesterol(mg)": cholesterol,
+                "carbohydrates": carbs,
+                "fiber(g)": fiber,
+                "sugars(g)": sugar,
+                "protein": protein
             }
             rows.append(itemDict)
     return rows
 
 
 rows = buildRows()
-print(rows)
+# print(rows)
 
 doltcli.write_rows(db, "menu_items", rows)
 
