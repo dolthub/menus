130c1f23e0ad618374e48e094fc6dc02141336f5
Katie McCulloch <katie@dolthub.com>
2021-07-11 10:48:08 -1000

    utils/validate: Clean up function names

--- validation/utils/strings.py
+++ validation/utils/strings.py
@@ -1,3 +1,7 @@
+from utils.print import *
+from static import *
+
+
 def stringPassesLeadingTrailingSpaceCheck(s):
     if s == "":
         return False
@@ -6,3 +10,12 @@ def stringPassesLeadingTrailingSpaceCheck(s):
 
 def stringIsUppercase(s):
     return s.upper() == s
+
+def validateString(colName, value):
+    if stringPassesLeadingTrailingSpaceCheck(value) is not True:
+      printInvalidSpace(colName, value)
+    if stringIsUppercase(value) is not True:
+      printInvalidUppercase(colName, value)
+    for char in invalid_chars:
+      if char in value:
+        printInvalidChar(colName, value, char)
--- validation/validate_pks.py
+++ validation/validate_pks.py
@@ -20,31 +20,9 @@ def validateIdentifier(identifier):
   if "," not in identifier:
     print(f'\nmenu_item.identifier "{identifier!r} must be equal to "NATIONAL", or in the format of <city>, <state>.\nExample: Santa Monica, CA\nIf the menu items are statewide, use NULL, <state>')
 
-def validatePkIsUppercase(colName, value):
-  if stringIsUppercase(value) is False:
-    printUppercaseCorrectionPrompt(colName, value)
-  
-def validatePkHasNoLeadingTrailingSpaces(colName, value):
-    if stringPassesLeadingTrailingSpaceCheck(value) is not True:
-      printLeadingTrailingSpaceCorrectionPrompt(colName, value)
-    if stringIsUppercase(value) is not True:
-      printUppercaseCorrectionPrompt(colName, value)
-
-def validateNoInvalidChars(colName, value):
-  for char in invalid_chars:
-    if char in value:
-      printInvalidCharCorrectionPrompt(colName, value, char)
-
-
-def validateCommonPkFormat(row):
-  for pk in menus_primary_keys:
-    validatePkIsUppercase(pk, row[pk])
-    validatePkHasNoLeadingTrailingSpaces(pk, row[pk])
-    validateNoInvalidChars(pk, row[pk])
-
-
 def validatePks(row):
-    validateCommonPkFormat(row)
+    for pk in menus_primary_keys:
+      validateString(pk, row[pk])
 
     # Tests specific formatting for the column
     validateName(row["name"])
